1dab2b24e2b374a2050a788142b3285033c55953
unknown <greentie@gmail.com>
2022-02-22 13:41:56 -0500

    initial commit - custom Car objects created

---
+++ force-app/main/default/lwc/carInventory/carInventory.js
@@ -0,0 +1,18 @@
+import { LightningElement } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import { NavigationMixin } from "lightning/navigation";
+
+import Model__c from "@salesforce/schema/Vehicle_Inventory__c";
+console.log("Model__c:", Model__c);
+
+export default class CarInventory extends NavigationMixin(LightningElement) {
+  createAccount() {
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: "Model__c",
+        actionName: "new"
+      }
+    });
+  }
+}

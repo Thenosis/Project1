49887fc71a83f85673976b4e7fd872b3b38f8550
Thenosis <martinlunaii18@gmail.com>
2022-02-23 17:34:36 -0500

    Fixed record form
    Fixed record form to render properly. Next commit will add picklist values.

--- OrderParts/force-app/main/default/lwc/orderParts/orderParts.js
+++ OrderParts/force-app/main/default/lwc/orderParts/orderParts.js
@@ -1,5 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+
+import PRODUCT_FIELD from '@salesforce/schema/OrderItem.Product2Id';
+import QUANTITY_FIELD from '@salesforce/schema/OrderItem.Quantity';
+
 
 export default class OrderParts extends LightningElement {
-    
+    @api OrderItem;
+
+    fields = [PRODUCT_FIELD, QUANTITY_FIELD];
 }

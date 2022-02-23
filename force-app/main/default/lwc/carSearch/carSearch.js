96d6ff9d75fea52ecf947e1205f688454c00958c
unknown <greentie@gmail.com>
2022-02-23 09:47:22 -0500

    car inventory styling updated

--- force-app/main/default/lwc/carSearch/carSearch.js
+++ force-app/main/default/lwc/carSearch/carSearch.js
@@ -3,10 +3,10 @@ import { LightningElement, track } from "lwc";
 export default class CarSearch extends LightningElement {
   @track carTypeId;
 
+  /* handler to update the result component with the changes */
   carTypeSelectHandler(e) {
     /* assign the carTypeId the value from detail
-    created in our custom event 
-    */
+    created in our custom event */
     this.carTypeId = e.detail;
   }
 }

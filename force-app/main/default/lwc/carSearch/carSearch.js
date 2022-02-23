2e2f63bd772c5ca228fd7c99929fe1712cf37cdf
unknown <greentie@gmail.com>
2022-02-23 13:36:55 -0500

    comments update

--- force-app/main/default/lwc/carSearch/carSearch.js
+++ force-app/main/default/lwc/carSearch/carSearch.js
@@ -1,7 +1,9 @@
 import { LightningElement, track } from "lwc";
 
 export default class CarSearch extends LightningElement {
-  @track carTypeId;
+  /* initialize an empty string so all cars 
+  show up by default on initial load */
+  @track carTypeId = "";
 
   /* handler to update the result component with the changes */
   carTypeSelectHandler(e) {

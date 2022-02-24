0da5ceaa9ec22fbc7a215fbd4019ff514eb2f53d
Thenosis <martinlunaii18@gmail.com>
2022-02-24 16:25:18 -0500

    Update to OrderParts
    Update to add other fields and comments to code.

--- OrderParts/force-app/main/default/lwc/orderParts/orderParts.js
+++ OrderParts/force-app/main/default/lwc/orderParts/orderParts.js
@@ -1,26 +1,40 @@
 import { LightningElement, api, wire, track } from 'lwc';
 
+//import of all fields from the Parts Order Custom Object
 import PARTS_ORDER from '@salesforce/schema/Parts_Order__c';
 import CAR from '@salesforce/schema/Parts_Order__c.Car__c';
 import CUSTOMER from '@salesforce/schema/Parts_Order__c.Customer__c';
+import PHONE from '@salesforce/schema/Parts_Order__c.Phone__c';
+import EMAIL from '@salesforce/schema/Parts_Order__c.Email__c';
+import ADDRESS from '@salesforce/schema/Parts_Order__c.Address__c';
 import ORDER_DATE from '@salesforce/schema/Parts_Order__c.Order_Date__c.';
 import PART from '@salesforce/schema/Parts_Order__c.Part__c';
 import QUANTITY from '@salesforce/schema/Parts_Order__c.Quantity__c';
 
-
+//export of the fields to the record form
 export default class OrderParts extends LightningElement {
     objectName= PARTS_ORDER;
     objectFields=[
         CUSTOMER,
+        PHONE,
+        EMAIL,
+        ADDRESS,
         CAR,
         ORDER_DATE,
         PART,
         QUANTITY
     ];
 
+    //event handler for when the cancel button is pressed
     handleCancel(event){}
+
+    //event handler for when the submit button is pressed
     handleSubmit(event){}
+
+    //event handler for when the record is successfully submitted
     handleSucess(event){}
+
+    //event handler for when the record throws an error during submission
     handleError(event){}
     
 
